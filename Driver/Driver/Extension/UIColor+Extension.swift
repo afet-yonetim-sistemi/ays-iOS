c3b6928b66c2cb4d6260761e356c1c74e6b0105e
Fatih Doğan <fatih.dogan@btpro.net>
2023-04-15 21:23:34 +0300

    Creating-CustomReusableButtonView: fix bugs

---
+++ Driver/Driver/Extension/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  Driver
+//
+//  Created by Fatih Doğan on 22.02.2023.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    static var appBlue: UIColor {
+        UIColor(named: "AppBlue") ?? UIColor(red: 31 / 255.0, green: 42 / 255.0, blue: 61/255.0, alpha: 1.0)
+    }
+}
