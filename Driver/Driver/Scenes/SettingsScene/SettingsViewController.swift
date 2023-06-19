56dd3dada47f63d041b785342096b3f0c07fff54
Ata Doruk <ata.doruk@hotmail.com>
2023-06-19 21:23:48 +0200

    Feature/Add Settings Screen: Add UI elements

--- Driver/Driver/Scenes/SettingsScene/SettingsViewController.swift
+++ Driver/Driver/Scenes/SettingsScene/SettingsViewController.swift
@@ -10,13 +10,23 @@ import UIKit.UIViewController
 
 final class SettingsViewController: UIViewController {
     
+    // MARK: Outlets
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var loadLabel: UILabel!
+    @IBOutlet weak var initialAddressLabel: UILabel!
+    @IBOutlet weak var finalAddressLabel: UILabel!
+    
+    @IBOutlet weak var feedbackButton: UIButton!
+    @IBOutlet weak var deleteAccountButton: UIButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    
     // MARK: Properties
     var viewModel: SettingsViewModelProtocol!
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
 
